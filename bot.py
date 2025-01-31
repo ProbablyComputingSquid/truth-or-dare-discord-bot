9fd4f2e9a00dbaf8d0ea1f1a350905ded96e892d
Jacob <jwbolling001@gmail.com>
2025-01-31 12:36:04 -0600

    push all changes because i forgot to actually update this

--- bot.py
+++ bot.py
@@ -1,26 +1,45 @@
 import discord
 from discord import app_commands
+from discord.ui import Button, View
 import os
+from random import randint
 from dotenv import load_dotenv
-from tord import tr, da
+from tord import truth, dare
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 
+async def truthEmbed(interaction):
+    info = truth()
+    embed=discord.Embed(title=f'{info["question"]}', color=0x58b9ff)
+    User = await client.fetch_user(int(interaction.user.id))
+    embed.set_author(name=f'Requested by: {User.display_name}', icon_url=User.avatar)
+    
+    embed.set_footer(text=f'Type: Truth | Rating: {info["rating"]} | ID: {info["id"]}')
+    return embed
+async def dareEmbed(interaction):
+    info = dare()
+    embed=discord.Embed(title=f'{info["question"]}', color=0x58b9ff)
+    User = await client.fetch_user(int(interaction.user.id))
+    embed.set_author(name=f'Requested by: {User.display_name}', icon_url=User.avatar)
+    
+    embed.set_footer(text=f'Type: Dare | Rating: {info["rating"]} | ID: {info["id"]}')
+    return embed
 # Add the guild ids in which the slash command will appear.
 # If it should be in all, remove the argument, but note that
 # it will take some time (up to an hour) to register the
 # command if it's for all guilds.
 GUILD = 1320526719626645504
 @tree.command(
-    name="commandname",
-    description="My first application Command",
+    name="ping",
+    description="Pong!",
     guild=discord.Object(id=GUILD)
 )
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+async def ping(interaction):
+    await interaction.response.send_message("Pong!")
+    
 @tree.command(
     name="profile",
     description="get user profile",
@@ -30,39 +49,80 @@ async def profile(interaction):
     User = await client.fetch_user(int(interaction.user.id))
     #print(User.avatar)
     await interaction.response.send_message(User.avatar)
+
+class EmbedButtons(View):
+    def __init__(self):
+        super().__init__()
+    
+    @discord.ui.button(label="Truth", style=discord.ButtonStyle.green, custom_id="truth_button")
+    async def truth_button_callback(self, interaction: discord.Interaction, button: Button):
+        await truthQuestion.callback(interaction)
+        await interaction.message.edit(view=None)
+    @discord.ui.button(label="Dare", style=discord.ButtonStyle.red, custom_id="dare_button")
+    async def dare_button_callback(self, interaction: discord.Interaction, button: Button):
+        await dareQuestion.callback(interaction)
+        await interaction.message.edit(view=None)
+    @discord.ui.button(label="Random", style=discord.ButtonStyle.blurple, custom_id="random_button")
+    async def hello_button_callback(self, interaction: discord.Interaction, button: Button):
+        if randint(0,1) == 0:
+            await truthQuestion.callback(interaction)
+        else:
+            await dareQuestion.callback(interaction)
+        await interaction.message.edit(view=None)
+
+
 @tree.command(
     name="embed_test",
     description="Embedding Test",
     guild=discord.Object(id=GUILD),
 )
 async def embedTest(interaction):
-    embed=discord.Embed(title="Question here")
+    info = truth()
+    embed=discord.Embed(title=f'{info["question"]}')
+    view=EmbedButtons()
     User = await client.fetch_user(int(interaction.user.id))
-    embed.set_author(name="Username", url=User.avatar)
-    embed.set_footer(text="Type: X, Rating: Y, ID: Z")
-    await ctx.send(embed=embed)
+    embed.set_author(name=f'Requested by:{User.display_name}', url=User.avatar)
+    
+    embed.set_footer(text=f'Type: Truth, Rating: {info["rating"]}, ID: {info["id"]}')
+
+    await interaction.response.send_message(embed=embed, view=view)
 
 
 @tree.command(
     name="truth",
-    description="Is it the truth?",
+    description="Get a random question that must be answered truthfully",
     guild=discord.Object(id=GUILD),
 )
-async def truth(interaction):
-    await interaction.response.send_message(tr())
+async def truthQuestion(interaction):
+    view = EmbedButtons()
+    embed = await truthEmbed(interaction)
+    await interaction.response.send_message(embed=embed, view=view)
 
 @tree.command(
     name="dare",
-    description="Do you dare?",
+    description="Do you dare to complete this task?",
     guild=discord.Object(id=GUILD),
 )
-async def dare(interaction):
-    await interaction.response.send_message(da())
-    
+async def dareQuestion(interaction):
+    view = EmbedButtons()
+    embed = await dareEmbed(interaction)
+    await interaction.response.send_message(embed=embed, view=view)
+@tree.command(
+    name="random",
+    description="Get a random Truth or Dare",
+    guild=discord.Object(id=GUILD),
+)
+async def randomQuestion(interaction):
+    if randint(0,1) == 0:
+        await truthQuestion.callback(interaction)
+    else:
+        await dareQuestion.callback(interaction)
+
+        
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=GUILD))
-    print("Ready!")
+    print("All Ready!")
 
 
 load_dotenv()
--- tord.py
+++ tord.py
@@ -1,18 +1,54 @@
 import random
 
-t = []
-d = []
+truths = []
+dares = []
+
 
 with open("questions/t.txt") as f:
     t = f.read().split("\n")
+    i = 0
+    rating = "PG"
+    for text in t:
+        temp = t[i].split("|")
+        truths.append({})
+        if len(temp) > 1:
+            rating = temp[0]
+            truths[i]["question"] = temp[1].strip()
+        else:
+            truths[i]["question"] = temp[0].strip()
+        truths[i]["rating"] = rating
+        i+=1
+
 with open("questions/d.txt") as f:
-    d = f.read().split("\n")
+    t = f.read().split("\n")
+    i = 0
+    rating = "PG"
+    for text in t:
+        temp = t[i].split("|")
+        dares.append({})
+        if len(temp) > 1:
+            rating = temp[0]
+            dares[i]["question"] = temp[1].strip()
+        else:
+            dares[i]["question"] = temp[0].strip()
+        dares[i]["rating"] = rating
+        i+=1
 
 #print(t)
 #print(d)
-def tr():
-    return t[random.randint(0, len(t) - 1)] + "?"
-def da():
-    return d[random.randint(0,len(d) - 1)]
+def truth():
+    qID = random.randint(0, len(truths) - 1)
+    return {
+        "question": truths[qID]["question"],
+        "id": qID,
+        "rating": truths[qID]["rating"],
+    }
+def dare():
+    qID = random.randint(0, len(dares) - 1)
+    return {
+        "question": dares[qID]["question"],
+        "id": qID,
+        "rating": dares[qID]["rating"],
+    }
 #print(tr())
 #print(da())
